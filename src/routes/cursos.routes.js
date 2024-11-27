832a7160faea5c10e6966ec003f6e8635f945b08
AnthoP4023 <anthonyp200330@gmail.com>
2024-11-27 08:20:27 -0500

    Update cursos.routes.js

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express'
-import {getCursos,getCursosxid} from '../controladores/cursos.Ctrl.js'
+import multer from 'multer';
+import {getCursos,getCursosxid,patchCurso} from '../controladores/cursos.Ctrl.js'
+
+// Configurar multer para almacenar en memoria
+const storage = multer.memoryStorage(); // Almacena imágenes en memoria
+const upload = multer({ storage });
+
 const router=Router()
 // armar nuestras rutas
 
 router.get('/cursos', getCursos) //select
 router.get('/cursos/:id', getCursosxid) //select x id
+router.patch('/cursos/:id', upload.single('image'), patchCurso); // Actualizar parcialmente un evento
 
 export default router 
