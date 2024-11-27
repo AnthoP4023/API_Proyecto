816990849646f6ec21422697cec2efa0ea5cff70
AnthoP4023 <anthonyp200330@gmail.com>
2024-11-27 07:13:03 -0500

    Update usuarios.routes.js

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -6,7 +6,7 @@ const router=Router()
 
 router.get('/usuarios', verifyToken, getUsuarios) //select
 router.get('/usuarios/:id', verifyToken, getUsarioxid); // Obtener un usuario por id (requiere token)
-router.post('/usuarios', verifyToken, postUsuarios)
-router.post('/login', verifyToken, login); // login
+router.post('/usuarios', postUsuarios)
+router.post('/login', login); // login
 
 export default router
